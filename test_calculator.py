ed0b83b860c6a8adae0ec557b5cdce574b9a5d66
Antiss88 <shetko.andrew@yndex.ru>
2024-02-03 22:49:18 +0300

    Print 88

---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+from calculator import Calculator
+
+
+
+#Задания
+#1.Разработчику 1 - реализовать функцию умножения
+#2. Разработчику 2 - реализовать функцию деления
+
+calc = Calculator()
+calc.sum(10,88)
+calc.print_last_res()
