119074ab1e8f21bd8edc0836bbac3c856ac779bc
Antiss88 <shetko.andrew@yndex.ru>
2024-02-03 22:49:18 +0300

    Шаблон калькулятора

--- test_calculator.py
+++ test_calculator.py
@@ -1,11 +1,6 @@
-from calculator import Calculator
+from calculator88 import Calculator
 
-
-
-#Задания
-#1.Разработчику 1 - реализовать функцию умножения
-#2. Разработчику 2 - реализовать функцию деления
 
 calc = Calculator()
-calc.sum(10,88)
+calc.sum(77 ,88)
 calc.print_last_res()
